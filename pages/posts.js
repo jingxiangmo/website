3fda7d428fbb898890bf12213945f66b6924451d
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-09-15 16:39:23 -0400

    netlify admin setup

--- pages/posts.js
+++ pages/posts.js
@@ -4,7 +4,7 @@ import { attributes, react as HomeContent } from '../content/posts/home.md';
 
 export default class Home extends Component {
   render() {
-    let { title, cats } = attributes;
+    let title = attributes;
     return (
       <>
         <Head>
