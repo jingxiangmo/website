4cb9525b67a9aec01be199614c353d85906b1770
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-09-16 17:57:31 -0400

    react routing created

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-
       {/*==========[ HEADER ]============*/}
       <Head>
         <title>Jingxiang Mo</title>
@@ -12,40 +12,42 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-
       {/*==========[ MAIN ]============*/}
       <main className={styles.main}>
-
         <div className={styles.heading}>
-          <h1 className={styles.title}> <u>JINGX</u>IANG MO </h1>
+          <h1 className={styles.title}>
+            {" "}
+            <u>JINGX</u>IANG MO{" "}
+          </h1>
 
-          <p className={styles.description}> 
-          building <a>softwares</a>, <a>robots</a>, <a>communities</a>
+          <p className={styles.description}>
+            building <a>softwares</a>, <a>robots</a>, <a>communities</a>
           </p>
         </div>
 
-
         <div className={styles.grid}>
-          <a href="" className={styles.card}>
-            <h2>posts &rarr;</h2>
-            <p> essays, book notes, and ideas </p>
-          </a>
+          <Link href={"/blog"}>
+            <a href="" className={styles.card}>
+              <h2>posts &rarr;</h2>
+              <p> essays, book notes, and ideas </p>
+            </a>
+          </Link>
 
-          <a href="" className={styles.card}>
-            <h2>projects &rarr;</h2>
-            <p> softwares, designs, and robots </p>
-          </a>
+          <Link href={"/projects"}>
+            <a href="" className={styles.card}>
+              <h2>projects &rarr;</h2>
+              <p> softwares, designs, and robots </p>
+            </a>
+          </Link>
 
-          <a
-            href=""
-            className={styles.card}
-          >
-            <h2>contact &rarr;</h2>
-            <p> email, resume, and github </p>
-          </a>
-
+          <Link href={"/contact"}>
+            <a href="" className={styles.card}>
+              <h2>contact &rarr;</h2>
+              <p> email, resume, and github </p>
+            </a>
+          </Link>
         </div>
       </main>
     </div>
-  )
+  );
 }
