af9c2e087c01980b4ade0ca20d1d659c590cfcd7
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-09-25 16:22:54 -0400

    Feature: layout doesn't apply to essays

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
 import '../styles/globals.scss'
 import Layout from '../components/layout'
+import {useRouter} from 'next/router';
 
 function MyApp ({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
-}
+  const router = useRouter();
+
+  if(router.asPath  != '/' && router.asPath != '/about' && router.asPath != '/projects' && router.asPath != '/writings' && router.asPath != '/contacts')  {
+     return (
+       <Component {...pageProps} />
+     )
+  }
+
+ return (
+   <Layout>
+     <Component {...pageProps} />
+   </Layout>
+ );
+ }
+
 
 export default MyApp
