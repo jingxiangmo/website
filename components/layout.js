4d51d310a9d70b4d2e876021101e0f624ef2e50d
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-10-08 19:01:55 -0400

    layout change

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ export default function Layout ({ children }) {
   return (
     <div className={styles.container}>
       <main className={styles.main}>{children}</main>
-      <Sidebar />
+      {/* <Sidebar /> */}
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,58 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.scss'
-import Link from 'next/link'
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import Link from "next/link";
 
-export default function Home () {
+export default function Home() {
   return (
     <div className={styles.container}>
-
       <Head>
         <title>Jingxiang Mo</title>
-        <meta name='Jingxiang Mo' content="jingxiang mo's website" />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="Jingxiang Mo" content="jingxiang mo's website" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.heading}>
-          <h1 className={styles.title}>
-            <u>JINGX</u>IANG MO{' '}
-          </h1>
+        <section className={styles.section}>
+          <div className={styles.title_block}>
+            <div className={styles.heading}>
+              <h1 className={styles.title}>
+                <u>JINGX</u>IANG MO{" "}
+              </h1>
 
-          <p className={styles.description}>
-            building <a>softwares</a>, <a>robots</a>, <a>communities</a>
-          </p>
-        </div>
+              <p className={styles.description}>
+                building <a>softwares</a>, <a>robots</a>, <a>communities</a>
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.section}>
+          {/* projects */}
+
+          <div className={style.section_block}>
+            <h1> projects </h1>
+            <p> </p>
+
+
+          </div>
+
+          {/* education */}
+          <h1> education </h1>
+        </section>
+
+        <section className={styles.section}>
+          {/* work */}
+          <h1> work </h1>
+
+          {/* contact */}
+          <h1> contact </h1>
+        </section>
+
+        <section className={styles.section}>
+          {/* writings */}
+          <h1> writings </h1>
+        </section>
       </main>
     </div>
-  )
+  );
 }
