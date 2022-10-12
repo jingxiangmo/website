3ff998410511f72c00af398784af8085300c4e1e
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-10-12 12:51:29 -0400

    clean up and new version

--- pages/index.js
+++ pages/index.js
@@ -36,151 +36,67 @@ export default class Home extends Component {
         </Head>
 
         <main className={styles.main}>
-          {/* ===========[ TITLE BLOCK ]===========  */}
-
-          <column className={styles.column}>
-            <div className={styles.title_column}>
-              <div className={styles.heading}>
-                <dt className={styles.title}> JINGXIANG MO </dt>
-
-                <p className={styles.description}>
-                  building softwares, robots, communities
-                </p>
-              </div>
+          {/* MAIN */}
+          <div>
+            <div className={styles.heading}>
+              <h1 className={styles.title}> JINGXIANG MO </h1>
+              <p className={styles.description}>
+                building softwares, robots, communities
+              </p>
             </div>
-          </column>
 
-          {/* ===========[ COLUMN 1]===========  */}
-          <column className={styles.column}>
-            {/* projects */}
 
-            <dl className={styles.block}>
-              <dt> PROJECTS </dt>
+            <section className={styles.section} id="projects">
+              <h2>projects</h2>
+              <p>…</p>
+            </section>
 
-              <dd>
-                <a href="">Robotics Research Project</a>
-                <br />
-                Branding, identity, strategy, web and digital design
-              </dd>
+            <section className={styles.section} id="contact">
+              <h2>contact </h2>
+              <p>…</p>
+            </section>
 
-              <dd>
-                <a href="">Streamline PoS</a>
-                <br />
-                Branding, identity, strategy, web and digital design
-              </dd>
+            <section className={styles.section} id="woks">
+              <h2>woks </h2>
+              <p>…</p>
+            </section>
 
-              <dd>
-                <a href="">Endevr</a>
-                <br />
-                Branding, identity, strategy, web and digital design
-              </dd>
+            <section className={styles.section} id="education">
+              <h2>education </h2>
+              <p>…</p>
+            </section>
 
-              <dd>
-                <a href="">Better Me</a>
-                <br />
-                Branding, identity, strategy, web and digital design
-              </dd>
+            <section className={styles.section} id="writings">
+              <h2>writings </h2>
+              <p>…</p>
 
-              <dd>
-                <a href="">Art Portfolio Website</a>
-                <br />
-                Branding, identity, strategy, web and digital design
-              </dd>
 
-              <dd>
-                <a href="">McGill Students' Society Website</a>
-                <br />
-                Branding, identity, strategy, web and digital design
-              </dd>
 
-              <dd>
-                <a href="">McGill Projects Community </a>
-                <br />
-                Branding, identity, strategy, web and digital design
-              </dd>
-            </dl>
-          </column>
 
-          {/* ===========[ COLUMN 2]===========  */}
 
-          <column className={styles.column}>
-            {/* contact */}
+            </section>
+          </div>
 
-            <dl className={styles.block}>
-              <dt> CONTACT </dt>
-              <dd>
-                <a href="">Email</a>
-              </dd>
-
-              <dd>
-                <a href="">Linkedin</a>
-              </dd>
-
-              <dd>
-                <a href="">Github</a>
-              </dd>
-            </dl>
-
-            {/* work */}
-            <dl className={styles.block}>
-              <dt> WORK </dt>
-
-              <dd>
-                Sep 2022 - Dec 2022
-                <br />
-                Reserach Assistant @ McGill Robotics Lab
-              </dd>
-
-              <dd>
-                Sep 2022 - Dec 2022
-                <br />
-                Product Engineer Intern @ Qubit (Coveo)
-              </dd>
-
-              <dd>
-                May 2022 - August 2022
-                <br />
-                Software Project Developer @ Pharmascience
-              </dd>
-
-              <dd>
-                Jan 2022 - May 2022
-                <br />
-                Co-Founder & Developer @ Streamline
-              </dd>
-            </dl>
-
-            {/* education */}
-            <dl className={styles.block}>
-              <dt> EDUCATION </dt>
-              <dd>
-                McGill University
-                <br />
-                Computer Science + Robotics + Finance
-              </dd>
-            </dl>
-          </column>
-
-          {/* ===========[ COLUMN 3]===========  */}
-
-          <column className={styles.column}>
-            {/* writings */}
-            <dl className={styles.block}>
-              <dt> WRITINGS </dt>
-            </dl>
-
-            <div className={styles.writings_list}>
-              {postsList.map((post) => {
-                return (
-                  <Link href={`writings/post/${post.slug}`}>
-                    <a>
-                      <h2>{post.attributes.title}</h2>
-                    </a>
-                  </Link>
-                );
-              })}
-            </div>
-          </column>
+          {/* NAVBAR */}
+          <nav className={styles.nav}>
+            <ol>
+              <li>
+                <a href="#contact">contact</a>
+              </li>
+              <li>
+                <a href="#projects">projects</a>
+              </li>
+              <li>
+                <a href="#work">work</a>
+              </li>
+              <li>
+                <a href="#education">education</a>
+              </li>
+              <li>
+                <a href="#writings">writings</a>
+              </li>
+            </ol>
+          </nav>
         </main>
       </div>
     );
