500f9cd1da8528a8405318d14f7a6f8f7ea160c3
Jingxiang Mo <65676392+jingxiangmo@users.noreply.github.com>
2022-11-03 19:38:05 -0400

    Fix google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,10 @@
 import '../styles/globals.scss'
-import Script from 'next/script'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 function MyApp ({ Component, pageProps }) {
   return (
     <>
-      <Script
-        strategy='afterInteractive'
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{
-          __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-                    page_path: window.location.pathname,
-                    });
-                `
-        }}
-      />
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </>
   )
