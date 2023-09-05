ecd103402e695a00ba2759eda508791f72f8569d
Jingxiang Mo <jingxiangmo@gmail.com>
2023-09-04 21:09:35 -0400

    Basic project compoent

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -27,8 +27,8 @@ const Projects: React.FC = () => {
                 <div className={styles.proj_card}>
                     <img className={styles.proj_img} src="/path/to/image.jpg" alt="Description" />
 
-                    <div >
-                        <div>
+                    <div className={styles.proj_info}>
+                        <div className={styles.proj_title_bar}>
                             <h1 className={styles.proj_title}> AlphaScript </h1>
                             <p className={styles.proj_date}> Aug 2023 </p>
                         </div>
@@ -37,11 +37,12 @@ const Projects: React.FC = () => {
 
                         <p className={styles.proj_description}> Hello world my name is Jacob, I love dogs. </p>
 
-                        <div className={styles.prof_links}>
-                            <p> Links: </p>
-                            <a href=""> Website </a>
-                            <a href=""> Github </a>
+                        <div className={styles.proj_links}>
+                            <p>Links:</p>
+                            <a className={styles.link} href="#">Website</a>
+                            <a className={styles.link}  href="#">Github</a>
                         </div>
+
                     </div>
                 </div>
 
