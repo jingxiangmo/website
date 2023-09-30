bec5b86114ade46b0de1e67a204c7eed5bdcd008
Jingxiang Mo <jingxiangmo@gmail.com>
2023-09-30 13:35:59 -0400

    Sanity CMS initalize

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import 'tailwindcss/tailwind.css'
+
+import 'styles/index.scss'
+import type { Metadata } from 'next'
+import { Manrope } from 'next/font/google'
+
+const font = Manrope({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Jingxiang mo',
+  description: 'Jingxiang Mo website. About, photos, projects.',
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      className={font.className}
+    >
+      <body>{children}</body>
+    </html>
+  )
+}
