890594f73b993f57540344500302ee16fc13a4d7
Jingxiang Mo <jingxiangmo@gmail.com>
2023-09-30 13:31:17 -0400

    Sanity CMS initalize

--- sanity/schema.ts
+++
@@ -1,9 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-
-import project from './schemas/project'
-import about from './schemas/about'
-import photo from './schemas/photo'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [project, about, photo],
-}
